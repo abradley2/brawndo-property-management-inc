30a626081cbbb865c4b80bd071bee74f0be19d6e
Tony Bradley <antbradley91@gmail.com>
2023-06-07 14:34:26 -0400

    Add missing filter paramter

--- server/api/agent/[id]/properties.get.ts
+++ server/api/agent/[id]/properties.get.ts
@@ -4,12 +4,14 @@ import type { Knex } from 'knex'
 export default defineEventHandler(async (event) => {
   const dbClient = event.context.db as Knex
 
+  const agentId = getRouterParam(event, 'id')
+
   const agentProperties = await dbClient
     .select('agent_property.id', 'agent_property.property_id', 'property.address', 'property.city', 'property.state', 'property.zip')
     .from('agent')
     .rightJoin('agent_property', 'agent.id', 'agent_property.agent_id')
     .rightJoin('property', 'agent_property.property_id', 'property.id')
-    .whereNotNull('agent_property.id')
+    .where('agent.id', parseInt(agentId))
 
   setResponseHeader(event, 'Content-Type', 'application/json')
   setResponseStatus(event, 200)
